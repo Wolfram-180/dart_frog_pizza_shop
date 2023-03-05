aa69a0c47205f9579ba1d356d43d42e5f02c952d
Wolfram-180 <sergei.ulvis@gmail.com>
2023-03-05 22:15:49 +0300

    pizza model

--- routes/index.dart
+++ routes/index.dart
@@ -1,5 +1,11 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Welcome to Dart Frog!');
+  // Access the incoming request.
+  final request = context.request;
+
+  // Access the HTTP method.
+  final method = request.method.value;
+
+  return Response(body: 'This is a $method request.');
 }
