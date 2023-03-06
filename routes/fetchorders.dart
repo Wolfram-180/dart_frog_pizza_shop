d44bd9ef7c651bb4ff85154032a90ffbeb3046b6
Wolfram-180 <sergei.ulvis@gmail.com>
2023-03-06 17:34:33 +0300

    mongo added

--- routes/fetchorders.dart
+++ routes/fetchorders.dart
@@ -1,43 +1,31 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
-//import 'package:dart_frog_pizza_shop/models/order_models.dart';
-import '../utils/constant.dart';
+import 'package:dart_frog_pizza_shop/models/order_models.dart';
+import '../services/database_services.dart';
 
-Response onRequest(RequestContext context) {
+Future<Response> onRequest(RequestContext context) async {
+  return DatabaseService.startConnection(context, getOrders(context));
+}
+
+Future<Response> getOrders(RequestContext context) async {
   //check if the request is a GET request
   if (context.request.method == HttpMethod.get) {
     //check if user_id is present
     final params = context.request.uri.queryParameters;
-    var userId = 0;
-    var isOrdersEmpty = '';
-
     if (params.containsKey('userId')) {
-      userId = int.parse(params['userId'] ?? '0');
-      final userOrders = orders.where(
-        (element) => element.userId == userId,
-      );
-
-      if (userOrders.isEmpty) {
-        isOrdersEmpty = 'isOrdersEmpty.isEmpty';
-      }
-
+      final userId = params['userId'];
+      final doc = await DatabaseService.ordersCollections
+          .find(where.eq('userId', userId))
+          .toList();
+      final userOrders = doc.map(OrderModel.fromJson).toList();
       if (userOrders.isNotEmpty) {
-        return Response.json(
-          body: {
-            'userId': userId,
-            'data': userOrders.toList(),
-          },
-        );
+        return Response.json(body: {'data': userOrders.toList()});
       }
     }
-    return Response.json(
-      body: {
-        'message':
-            'User id = ${userId.runtimeType} $userId not found in Orders : $isOrdersEmpty',
-      },
-    );
+    return Response.json(body: {'message': 'User id not found'});
   }
   return Response.json(
     statusCode: 404,
